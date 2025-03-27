947df5e85606f1b590bb213945da30caf4eaa207
Prafulpradhan11 <Prafulpradhan1113@gamil.com>
2025-03-27 12:54:59 +0545

    Setup First File

---
+++ src/Button.jsx
@@ -0,0 +1,15 @@
+
+
+const Button = ({ buttonVitrakoContent, arkoContent}) => {
+    return (
+        <>
+            <button>{buttonVitrakoContent}</button>
+            {/* <button>{arkoContent}</button> */}
+        </>
+    );
+};
+
+export default Button;
+
+
+
