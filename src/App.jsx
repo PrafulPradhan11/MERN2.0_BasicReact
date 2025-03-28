4b9704159e3d4d99738b8d382a698c554b0cdd62
Prafulpradhan11 <Prafulpradhan1113@gamil.com>
2025-03-28 13:33:38 +0545

    routing with-{createBrowserRouter} and basic of useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Home from './Home'
-import About from './About'
-import Contact from './Contact'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes'
+import { useState } from 'react'
 
 function App() {
-  return (
-   <BrowserRouter>
-       <Routes>
-          <Route path= '/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-        </Routes>
-   </BrowserRouter>
-  
+// var resultReturnedFromUseState = useState(0)
+// var firstIndexValue = resultReturnedFromUseState[0]
+// var secondIndexFunction = resultReturnedFromUseState[1]
+  const[count,setCount] = useState(0)
+  const [name,setName] = useState("Picaso")
 
+  return (
+      //  <RouterProvider router={router}/>
+    <>  
+     {/* <h1>Learning UseState</h1>
+      <h1>{firstIndexValue}</h1>
+      <button onClick={()=>secondIndexFunction(firstIndexValue + 2)}>+</button>    */}
+    <h1>{count}</h1>
+    <button onClick={()=>setCount(count + 1)}>+</button>
+    <button onClick={()=>setCount(count - 1)}>-</button>
+    <h1>{name}</h1>
+    <button onClick={()=>setName("Praful")}>Change Name</button>
+    </>
   )
 }
 
