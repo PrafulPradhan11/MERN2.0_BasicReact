4b4b93a6a6cf3f64342fdf54a4ced9296c070ba7
Prafulpradhan11 <Prafulpradhan1113@gamil.com>
2025-04-03 13:24:20 +0545

    Useeffect hook setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
 import { RouterProvider } from 'react-router-dom'
 import router from './routes'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
-// var resultReturnedFromUseState = useState(0)
-// var firstIndexValue = resultReturnedFromUseState[0]
-// var secondIndexFunction = resultReturnedFromUseState[1]
-  const[count,setCount] = useState(0)
-  const [name,setName] = useState("Picaso")
+  const [count,setCount] = useState(0)
+  const [nextCount,setNextCount] = useState(0)
+  
+  //first type
+  // useEffect(()=>{
+  //   console.log("Not count wala useEffect")               
+  // },[])
+
+  // Second type
+  // useEffect(()=>{
+  //   console.log("Count wala UseEffect")
+  //   document.title = "Learning Useeffect" + count
+  // }, [count])
+
+  //Third type
+  // useEffect(()=>{
+  //  console.log("Third useEffect was called")
+  // })
 
   return (
-      //  <RouterProvider router={router}/>
-    <>  
-     {/* <h1>Learning UseState</h1>
-      <h1>{firstIndexValue}</h1>
-      <button onClick={()=>secondIndexFunction(firstIndexValue + 2)}>+</button>    */}
+    <>
     <h1>{count}</h1>
-    <button onClick={()=>setCount(count + 1)}>+</button>
-    <button onClick={()=>setCount(count - 1)}>-</button>
-    <h1>{name}</h1>
-    <button onClick={()=>setName("Praful")}>Change Name</button>
+    <button onClick={()=>setCount(count+1)}>+</button>
+    <h1>{nextCount}</h1>
+    <button onClick={()=>setNextCount(nextCount+1)}>+</button>
     </>
   )
 }
